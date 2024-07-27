20765c65b8c27ee8a66a23fa125fcc5b9fabc754
Leonardo <leoppasseri@gmail.com>
2024-07-27 10:57:11 -0300

    Criação da classe Posição

--- Sistema_Xadrez/Aplicação/Programa.java
+++ Sistema_Xadrez/Aplicação/Programa.java
@@ -1,7 +1,10 @@
 package Sistema_Xadrez.Aplicação;
 
+import Sistema_Xadrez.Tabuleiro.*;
+
 public class Programa {
     public static void main(String[] args) {
-       
+        Posição pos = new Posição(3,5);
+        System.out.println(pos);
     }
 }
---
+++ Sistema_Xadrez/Tabuleiro/Posição.java
@@ -0,0 +1,27 @@
+package Sistema_Xadrez.Tabuleiro;
+
+public class Posição {
+    private int linha;
+    private int coluna;
+
+    public Posição(){
+    }
+    public Posição(int linha,int coluna){
+        this.coluna = coluna;
+        this.linha = linha;
+    }
+    @Override
+    public String toString(){
+        return linha + ", " + coluna;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }public int getColuna() {
+        return coluna;
+    }public void setLinha(int linha) {
+        this.linha = linha;
+    }public int getLinha() {
+        return linha;
+    }
+}
