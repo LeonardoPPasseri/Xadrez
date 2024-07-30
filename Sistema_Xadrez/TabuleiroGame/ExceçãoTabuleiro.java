6ebe9b2eb284c6ea6ea218115afb098ed8adca2c
Leonardo <leoppasseri@gmail.com>
2024-07-30 12:54:01 -0300

    Criação da exceção personalizada para classe Tabuleiro

---
+++ Sistema_Xadrez/TabuleiroGame/ExceçãoTabuleiro.java
@@ -0,0 +1,8 @@
+package Sistema_Xadrez.TabuleiroGame;
+
+public class ExceçãoTabuleiro extends RuntimeException{
+    
+    public ExceçãoTabuleiro(String msg){
+        super(msg);
+    }
+}
--- Sistema_Xadrez/TabuleiroGame/Tabuleiro.java
+++ Sistema_Xadrez/TabuleiroGame/Tabuleiro.java
@@ -8,28 +8,53 @@ public class Tabuleiro {
     public Tabuleiro(){
     }
     public Tabuleiro(int linhas,int colunas){
+        if(linhas < 1 || colunas < 1){
+            throw new ExceçãoTabuleiro("Erro na Criação do tabuleiro: O tabuleiro deve ter pelo menos 1 linha e 1 coluna");
+        }
         this.colunas = colunas;
         this.linhas = linhas;
         peças = new Peça[linhas][colunas];
     }
 
     public Peça peças(int linha,int coluna){
+        if(!existePosição(linha,coluna)){
+            throw new ExceçãoTabuleiro("Posição inexistente no tabuleiro");
+        }
         return peças[linha][coluna];
     }
     public Peça peças(Posição posição){
+        if(!existePosição(posição)){
+            throw new ExceçãoTabuleiro("Posição inexistente no tabuleiro");
+        }
         return peças[posição.getLinha()][posição.getColuna()];
     }
     public void lugarPeça(Peça peça, Posição posição){
+        if(temUmaPeça(posição)){
+            throw new ExceçãoTabuleiro("Ja existe uma peça na posição (" + posição + ")");
+        }
         peças[posição.getLinha()][posição.getColuna()] = peça;
         peça.posição = posição;
     }
 
-    public void setColunas(int colunas) {
-        this.colunas = colunas;
-    }public int getColunas() {
+
+    public boolean existePosição(int linha,int coluna){
+        return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+                
+    }
+    public boolean existePosição(Posição posição){
+        return existePosição(posição.getLinha(),posição.getColuna());
+    }
+    public boolean temUmaPeça(Posição posição){
+        if(!existePosição(posição)){
+            throw new ExceçãoTabuleiro("Posição inexistente no tabuleiro");
+        }
+        return peças(posição) != null;
+    }
+
+
+   
+    public int getColunas() {
         return colunas;
-    }public void setLinhas(int linhas) {
-        this.linhas = linhas;
     }public int getLinhas() {
         return linhas;
     }
